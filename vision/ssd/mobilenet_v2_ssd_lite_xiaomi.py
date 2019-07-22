ece483b4cb4943757118328c46e0fd63e3ca21a4
liqingyuan <liqingyuan@xiaomi.com>
2019-07-22 07:19:44 -0400

    add dropout2d for classification header

--- vision/ssd/mobilenet_v2_ssd_lite_xiaomi.py
+++ vision/ssd/mobilenet_v2_ssd_lite_xiaomi.py
@@ -1,5 +1,5 @@
 import torch
-from torch.nn import Conv2d, Sequential, ModuleList, BatchNorm2d
+from torch.nn import Conv2d, Sequential, ModuleList, BatchNorm2d, Dropout2d
 from torch import nn
 from ..nn.mobilenet_v2_xiaomi import MobileNetV2, InvertedResidual
 
@@ -15,6 +15,19 @@ def SeperableConv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=
     return Sequential(
         Conv2d(in_channels=in_channels, out_channels=in_channels, kernel_size=kernel_size,
                groups=in_channels, stride=stride, padding=padding),
+        BatchNorm2d(in_channels),
+        ReLU(),
+        Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1),
+    )
+
+def SeperableConv2dDropout(in_channels, out_channels, kernel_size=1, stride=1, padding=0, onnx_compatible=False):
+    """Replace Conv2d with a depthwise Conv2d and Pointwise Conv2d.
+    """
+    ReLU = nn.ReLU if onnx_compatible else nn.ReLU6
+    return Sequential(
+        Conv2d(in_channels=in_channels, out_channels=in_channels, kernel_size=kernel_size,
+               groups=in_channels, stride=stride, padding=padding),
+        Dropout2d(p=0.2),
         BatchNorm2d(in_channels),
         ReLU(),
         Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1),
@@ -46,11 +59,11 @@ def create_mobilenetv2_ssd_lite_xiaomi(num_classes, width_mult=1.0, use_batch_no
     ])
 
     classification_headers = ModuleList([
-        SeperableConv2d(in_channels=round(576 * width_mult), out_channels=6 * num_classes, kernel_size=3, padding=1),
-        SeperableConv2d(in_channels=1280, out_channels=6 * num_classes, kernel_size=3, padding=1),
-        SeperableConv2d(in_channels=512, out_channels=6 * num_classes, kernel_size=3, padding=1),
-        SeperableConv2d(in_channels=256, out_channels=6 * num_classes, kernel_size=3, padding=1),
-        SeperableConv2d(in_channels=256, out_channels=6 * num_classes, kernel_size=3, padding=1),
+        SeperableConv2dDropout(in_channels=round(576 * width_mult), out_channels=6 * num_classes, kernel_size=3, padding=1),
+        SeperableConv2dDropout(in_channels=1280, out_channels=6 * num_classes, kernel_size=3, padding=1),
+        SeperableConv2dDropout(in_channels=512, out_channels=6 * num_classes, kernel_size=3, padding=1),
+        SeperableConv2dDropout(in_channels=256, out_channels=6 * num_classes, kernel_size=3, padding=1),
+        SeperableConv2dDropout(in_channels=256, out_channels=6 * num_classes, kernel_size=3, padding=1),
         Conv2d(in_channels=64, out_channels=6 * num_classes, kernel_size=1),
     ])
 
