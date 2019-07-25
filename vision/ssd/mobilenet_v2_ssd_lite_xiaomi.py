507a38f2ef834a5e8bea8f47601b605421856b2d
liqingyuan <13817189216@163.com>
2019-07-25 06:38:08 +0000

    remove dropout

--- vision/ssd/mobilenet_v2_ssd_lite_xiaomi.py
+++ vision/ssd/mobilenet_v2_ssd_lite_xiaomi.py
@@ -59,11 +59,11 @@ def create_mobilenetv2_ssd_lite_xiaomi(num_classes, width_mult=1.0, use_batch_no
     ])
 
     classification_headers = ModuleList([
-        SeperableConv2dDropout(in_channels=round(576 * width_mult), out_channels=6 * num_classes, kernel_size=3, padding=1),
-        SeperableConv2dDropout(in_channels=1280, out_channels=6 * num_classes, kernel_size=3, padding=1),
-        SeperableConv2dDropout(in_channels=512, out_channels=6 * num_classes, kernel_size=3, padding=1),
-        SeperableConv2dDropout(in_channels=256, out_channels=6 * num_classes, kernel_size=3, padding=1),
-        SeperableConv2dDropout(in_channels=256, out_channels=6 * num_classes, kernel_size=3, padding=1),
+        SeperableConv2d(in_channels=round(576 * width_mult), out_channels=6 * num_classes, kernel_size=3, padding=1),
+        SeperableConv2d(in_channels=1280, out_channels=6 * num_classes, kernel_size=3, padding=1),
+        SeperableConv2d(in_channels=512, out_channels=6 * num_classes, kernel_size=3, padding=1),
+        SeperableConv2d(in_channels=256, out_channels=6 * num_classes, kernel_size=3, padding=1),
+        SeperableConv2d(in_channels=256, out_channels=6 * num_classes, kernel_size=3, padding=1),
         Conv2d(in_channels=64, out_channels=6 * num_classes, kernel_size=1),
     ])
 
